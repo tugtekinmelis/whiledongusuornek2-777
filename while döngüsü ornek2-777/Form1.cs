76155584bbd1afac6832149f9c1fd703a0ea5330
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-21 12:32:48 +0300

    Add files via upload

---
+++ while döngüsü ornek2-777/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_döngüsü_ornek2_777
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dongu_say = 1;
+            int toplam = 0;
+            while (toplam<=1000)
+            {
+                toplam = toplam + dongu_say;
+                dongu_say += 1;
+            }
+            MessageBox.Show(" Döngü toplam " + dongu_say.ToString() + " kez çalıştı ");
+        }
+    }
+}
